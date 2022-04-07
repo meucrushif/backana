3da294e98c5b0fa01babccca5b0936f25e89261d
meucrushif <meucrushif@gmail.com>
2022-04-07 19:51:17 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f3493d0bb03e6e5625825e5313ead44c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '59bd1765-024b-466f-8088-d9053818d404'
+};
